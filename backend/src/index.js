806e595b9d860b25d777d65ff1a87157c6eb1a3e
meeran11 <meeran.official1255@gmail.com>
2026-01-02 16:04:15 +0500

    Vercel Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,6 +29,9 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/user', userRoutes);
 app.use('/car', carRoutes);
 app.use('/api', maintenanceRoutes);
